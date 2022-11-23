4470647c0e2e91209e4147928edb9669429f1e4a
Mark Krautheim <mark.krautheim@gmail.com>
2022-11-23 02:42:09 +0000

    Update commands to use allinone image

--- run_app.sh
+++ run_app.sh
@@ -5,5 +5,5 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 docker run --rm \
   -v $SCRIPT_PATH/data:/data \
   -v $SCRIPT_PATH/src:/app \
-  tesla_gateway_data
+  allinone-py311 /bin/bash get_tesla_gateway_meter_data.sh
 
--- run_bash.sh
+++ run_bash.sh
@@ -5,4 +5,4 @@ SCRIPT_PATH=$(dirname $(realpath -s $0))
 docker run --rm -it \
   -v $SCRIPT_PATH/data:/data \
   -v /tmp:/sunpower \
-  tesla_gateway_data /bin/bash
+  allinone-py311 /bin/bash
